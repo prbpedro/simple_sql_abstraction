d95591ceef62f47764c8586067516d228a2a3fde
prbpedro <prbpedro>
2019-08-30 14:16:36 -0300

    end

--- tests/test_module.py
+++ tests/test_module.py
@@ -12,7 +12,7 @@ class ModuleTest(unittest.TestCase):
 
     def test(self):
         try:
-            data_base_location = 'simple_flask_peewee_db.db'
+            data_base_location = 'database.db'
             dao.ConnectionManager(data_base_location, test_entity.__name__,
                                   test_entity.DDLCOMMAND, in_memory=False,
                                   clear_data_base=True)
@@ -49,5 +49,4 @@ class ModuleTest(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
